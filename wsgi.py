99b95c5b030a4333f7d0530026f0b1afeaee96d9
q1706188538 <q1706188538@qq.com>
2025-05-05 00:31:09 +0800

    修复 Gunicorn 启动问题：更新 wsgi.py 文件

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
-from app import app
+# 直接导入 app.py 中的 app 变量
+import app as flask_app
+
+# 获取 Flask 应用程序实例
+application = flask_app.app
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    serve(application, host='0.0.0.0', port=5000)
